b8fafbe8a5cb43bbdf7481b5ade69ae9c0cd751c
Kamlesh718 <kamleshvishwakarma1202@gmail.com>
2025-07-08 20:22:40 +0530

    newsAPI initialization

---
+++ index.js
@@ -0,0 +1,31 @@
+const { configDotenv } = require("dotenv");
+const express = require("express");
+const cors = require("cors");
+
+configDotenv();
+
+const app = express();
+const port = 3000;
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
+
+const newsAPIKey = process.env.NEWS_API_KEY;
+// console.log(newsAPIKey);
+app.get("/api/news", async (req, res) => {
+  try {
+    const page = req.query.page || 1;
+    const apiRes = await fetch(
+      `https://newsapi.org/v2/everything?q=india&language=en&pageSize=8&page=${page}&apiKey=${newsAPIKey}`
+    );
+    const data = await apiRes.json();
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json({ error: "News API error" });
+  }
+});
+
+app.listen(port, () => console.log(`Server is running on port 3000`));
